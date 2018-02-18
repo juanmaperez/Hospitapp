c11f942a34171d0f89afd3e83a5b2b705f8097c9
juanmaperez <juanmaperezvar@gmail.com>
2018-02-18 21:55:40 +0100

    register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { Graphics1Component } from './pages/graphics1/graphics1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -25,7 +27,9 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     Graphics1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,24 @@ import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graphics1Component } from './pages/graphics1/graphics1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './register/register.component';
+
 
 const routes : Routes = [
-    { path: 'dashboard', component: DashboardComponent},
+    { 
+        path:'', 
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent},
+            { path: 'graphics', component: Graphics1Component},
+            { path: 'progress', component: ProgressComponent},
+            { path: '', redirectTo:'/', pathMatch: 'full'},
+        ]
+    },
     { path: 'login', component: LoginComponent},
-    { path: 'register', component: LoginComponent},
-    { path: 'graphics', component: Graphics1Component},
-    { path: 'progress', component: ProgressComponent},
-    { path: '', redirectTo:'/dashboard', pathMatch: 'full'},
+    { path: 'register', component: RegisterComponent},
+
     { path: '**', component: NopagefoundComponent},
 ]
 
