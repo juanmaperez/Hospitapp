f4d3ba46ca61bc2334328ccade196f9d6406ef14
juanmaperez <juanmaperezvar@gmail.com>
2018-03-04 23:05:00 +0100

    fix incrementator

--- src/app/components/incrementator/incrementator.component.ts
+++ src/app/components/incrementator/incrementator.component.ts
@@ -7,44 +7,42 @@ import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef} 
 })
 export class IncrementatorComponent implements OnInit {
 
-  @Input() progress: number = 50;
-  @Input() title: string = 'Title'
+  @Input() progress: Number = 50;
+  @Input() title: String = 'Title';
 
-  @Output() increment : EventEmitter<number> = new EventEmitter();
+  @Output() increment: EventEmitter<Number> = new EventEmitter();
 
-  @ViewChild('inputProgress') inputProgress: ElementRef
+  @ViewChild('inputProgress') inputProgress: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onChanges(value: number){
+  onChanges(value: Number) {
 
-    if(value >= 100){
+    if (value >= 100) {
       this.progress = 100;
-    } else if ( value <= 0){
-      this.progress = 0
+    } else if ( value <= 0) {
+      this.progress = 0;
     } else {
-      this.progress = value
+      this.progress = value;
     }
     this.inputProgress.nativeElement.value = this.progress;
-    this.increment.emit(this.progress)
+    this.increment.emit(this.progress);
   }
 
-  changeValue (valor): void{
-    if(this.progress <= 100 && this.progress >= 0){
-       this.progress += valor 
+  changeValue (valor): void {
+    if (this.progress <= 100 && this.progress >= 0) {
+       this.progress += valor;
     }
-    this.progress = (this.progress>100)? 100 : this.progress 
-    this.progress = (this.progress<0)? 0 : this.progress 
+    this.progress = (this.progress > 100) ? 100 : this.progress;
+    this.progress = (this.progress < 0) ? 0 : this.progress;
 
     this.increment.emit(this.progress);
     this.inputProgress.nativeElement.focus();
 
   }
 
-    
-  }
-
 }
+
